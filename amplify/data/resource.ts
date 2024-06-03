09a482ed2d0f2edea36a2621c1f3c707f7621f79
Alan Terriaga <alanst32@gmail.com>
2024-06-03 16:13:52 +1000

    Added authenticator

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -11,7 +11,8 @@ const schema = a.schema({
     .model({
       content: a.string(),
     })
-    .authorization((allow) => [allow.publicApiKey()]),
+    .authorization((allow) => [allow.owner()]),
+  // Isolate the data as "onwer-based-authorizatoin" per user basis
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,7 +20,9 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    // This tells the data client in your app (generateClient())
+    // to sign API requests with the user authentication token.
+    defaultAuthorizationMode: "userPool",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
