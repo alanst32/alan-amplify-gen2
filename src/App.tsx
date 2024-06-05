d4f8489ec0dfb7c8addc69be0d31d73967b8dabc
Alan Terriaga <alanst32@gmail.com>
2024-06-05 15:38:47 +1000

    example

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,7 @@ import Header from './custom-components/header';
 import Footer from './custom-components/footer';
 import { signOut } from 'aws-amplify/auth';
 
-// const client = generateClient<Schema>();
-
 function App() {
-  // const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  // useEffect(() => {
-  //   client.models.Todo.observeQuery().subscribe({
-  //     next: (data) => setTodos([...data.items]),
-  //   });
-  // }, []);
-
-  // function createTodo() {
-  //   client.models.Todo.create({ content: window.prompt("Todo content") });
-  // }
-
-  // function deleteTodo(id: string) {
-  //   client.models.Todo.delete({ id });
-  // }
-
   async function handleSignOut() {
     await signOut();
   }
@@ -34,14 +16,6 @@ function App() {
         <Header></Header>
         <EventLanding></EventLanding>
         <Footer signOut={handleSignOut}></Footer>
-        {/* <button onClick={createTodo}>+ new</button>
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-                {todo.content}
-              </li>
-            ))}
-          </ul> */}
       </main>
     </Authenticator>
   );
